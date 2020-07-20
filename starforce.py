063f9d6298f88df02a1f9c4711a4d478517a2a16
Axarious <58768330+Axariosu@users.noreply.github.com>
2020-07-20 15:18:41 -0700

    update before migrating to browser

--- starforce.py
+++ starforce.py
@@ -7,9 +7,9 @@ import matplotlib.animation as animation
 
 # global variables
 __INITSTAR__ = 17
-__GOAL__ = 25
+__GOAL__ = 23
 __EQUIPMENTLEVEL__ = 150
-__SIMULATIONS__ = 10 ** 4
+__SIMULATIONS__ = 10 ** 5
 
 success_arr = [0.95, 0.90, 0.85, 0.85, 0.8, 0.75, 0.70, 0.65, 0.60, 0.55, 0.50, 0.45, 0.40, 0.35, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.03, 0.02, 0.01]
 destroy_arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.01, 0.02, 0.03, 0.03, 0.03, 0.03, 0.04, 0.04, 0.10, 0.10, 0.20, 0.30, 0.40]
@@ -37,32 +37,28 @@ for i in range(__SIMULATIONS__):
             star += 1
             failstack = 0    
         # normal starring
+        if random.random() < success_arr[star]:
+            star += 1
+            failstack = 0
+        # failed logic 
         else: 
-            # if successful, star += 1
-            if random.random() < success_arr[star]:
-                star += 1
-                failstack = 0
-            # failed logic 
-            else: 
-                # fail maintain logic
-                # check if boomed
-                if random.random() < destroy_arr[star]:
-                    # 18 and higher cannot safeguard
-                    if safeguard and star < 17:
-                        star -= 1
-                        failstack +=1 
-                    else: 
-                        destroyed = True
-                        boom += 1 
+            # fail maintain logic
+            # check if boomed
+            if random.random() < destroy_arr[star]:
+                # 18 and higher cannot safeguard
+                if safeguard and star < 17:
+                    star -= 1
+                    failstack +=1 
                 else: 
-                    # checkpoints
-                    if star <= 10 or star == 15 or star == 20: 
-                        failstack += 1
-                        continue
-                    else: 
-                        star -= 1
-                        failstack += 1
-        # print(star)
+                    destroyed = True
+                    boom += 1 
+            else: 
+                # checkpoints
+                if star <= 10 or star == 15 or star == 20: 
+                    failstack += 1
+                else: 
+                    star -= 1
+                    failstack += 1
     results[star] += 1
     star = __INITSTAR__
     destroyed = False
