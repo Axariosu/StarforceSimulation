49816a8d53bee8fe9894d51bf72ba5c34689209c
Axarious <58768330+Axariosu@users.noreply.github.com>
2020-07-28 20:26:23 -0700

    bugfix

--- starforce.py
+++ starforce.py
@@ -14,10 +14,10 @@ import time, random
 #   if not, then roll again for destroy chance
 
 # global variables
-__INITSTAR__ = 10
+__INITSTAR__ = 20
 __GOAL__ = 22
-__EQUIPMENTLEVEL__ = 140
-__SIMULATIONS__ = 1 * 10 ** 0
+__EQUIPMENTLEVEL__ = 150
+__SIMULATIONS__ = 1 * 10 ** 4
 
 star = __INITSTAR__
 goal = __GOAL__
@@ -35,9 +35,9 @@ count = 0
 min_meso = 10 ** 22
 max_meso = 0
 # normal starring array: 
-# success_arr = [0.95, 0.90, 0.85, 0.85, 0.8, 0.75, 0.70, 0.65, 0.60, 0.55, 0.50, 0.45, 0.40, 0.35, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.03, 0.02, 0.01]
+success_arr = [0.95, 0.90, 0.85, 0.85, 0.8, 0.75, 0.70, 0.65, 0.60, 0.55, 0.50, 0.45, 0.40, 0.35, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.03, 0.02, 0.01]
 # star_catch success_arr:
-success_arr = [0.99275, 0.9405, 0.88825, 0.88825, 0.836, 0.78375, 0.7315, 0.67925, 0.627, 0.57475, 0.5225, 0.47025, 0.418, 0.36575, 0.3135, 0.3135, 0.3135, 0.3135, 0.3135, 0.3135, 0.3135, 0.3135, 0.03135, 0.0209, 0.01045]
+# success_arr = [0.99275, 0.9405, 0.88825, 0.88825, 0.836, 0.78375, 0.7315, 0.67925, 0.627, 0.57475, 0.5225, 0.47025, 0.418, 0.36575, 0.3135, 0.3135, 0.3135, 0.3135, 0.3135, 0.3135, 0.3135, 0.3135, 0.03135, 0.0209, 0.01045]
 # 5_10_15 success_arr: 
 # success_arr = [0.95, 0.90, 0.85, 0.85, 0.8, 1, 0.70, 0.65, 0.60, 0.55, 1, 0.45, 0.40, 0.35, 0.30, 1, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.03, 0.02, 0.01]
 # star_catch success_arr during 5/10/15, given by [1.045 * i for i in success_arr] and float errors removed:
@@ -69,6 +69,7 @@ meso_array = [1000 + __EQUIPMENTLEVEL__ ** 3 * (0 + 1) / 25,
     1000 + __EQUIPMENTLEVEL__ ** 3 * (23 + 1) ** 2.7 / 100, 
     1000 + __EQUIPMENTLEVEL__ ** 3 * (24 + 1) ** 2.7 / 100]
 meso_array = [int(i) for i in meso_array]
+# print(meso_array)
 
 # initialize results dict
 if __name__ == "__main__":
@@ -78,14 +79,16 @@ if __name__ == "__main__":
 
     start_time = time.time()
     for i in range(__SIMULATIONS__): 
+    # while count < __SIMULATIONS__:
+        # print()
         while not destroyed and star != goal:
-            # print(star, failstack)
+            # print(meso, star, failstack)
             # chance time
             if failstack == 2: 
                 meso += meso_array[star]
                 star += 1
                 failstack = 0
-                break
+                # print(meso, star, failstack)
             # normal starring
             meso += meso_array[star]
             if random.random() < success_arr[star]:
@@ -99,7 +102,7 @@ if __name__ == "__main__":
                     # 18 and higher cannot safeguard
                     if safeguard and star < 17:
                         star -= 1
-                        failstack +=1 
+                        failstack += 1 
                     else: 
                         destroyed = True
                         boom += 1 
@@ -110,17 +113,18 @@ if __name__ == "__main__":
                     else: 
                         star -= 1
                         failstack += 1
-        
+
         results[star] += 1
 
         if not destroyed: 
             min_meso = min(meso, min_meso)
             max_meso = max(meso, max_meso)
-        meso_avg = (meso + count * meso_avg) / (count + 1)
+            meso_avg = (meso + count * meso_avg) / (count + 1)
         
         count = count + 1 if not destroyed else count
         star = __INITSTAR__ if not destroyed else 12
         meso = 0 if not destroyed else meso
+        
         destroyed = False
     for i in range(12,26):
         print(i, results[i])
